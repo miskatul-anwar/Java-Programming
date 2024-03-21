ddf1d914d8b373ffa54c5ef33f6e5194f58ad645
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-03-21 13:29:19 +0000

    Thu Mar 21 01:29:19 PM UTC 2024

---
+++ literals/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+  public static void main(String[] args) {
+    byte age = 34;
+    char gender = 'M';
+    float height = 5.4f;
+    double weight = 34.5D;
+    System.out.println(age);
+  }
+}
