88860ac535241500250b27b1194f3b1764ef2b50
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-05-20 07:02:16 +0600

    Mon May 20 07:02:16 AM +06 2024

--- array.java
+++ array.java
@@ -5,11 +5,11 @@ public class array {
     Scanner sc = new Scanner(System.in);
     int n = sc.nextInt();
     int a[] = new int[n];
-    for (int i = 0; i < n; i++) {
+    for (int i = 0; i < a.length; i++) {
       a[i] = sc.nextInt();
     }
     sc.close();
     System.out.println("Array elements are: " + Arrays.toString(a));
-    System.out.printf("Maximum element is: %d\n", a[n - 1]);
+    System.out.printf("Maximum element is: %d", a[n - 1]);
   }
 }
