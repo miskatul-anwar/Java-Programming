8b12c2bf74ae063d06a3770990ff4fc04b13af0a
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-05-21 10:15:01 +0600

    Tue May 21 10:15:01 AM +06 2024

---
+++ getters_setters.cpp
@@ -0,0 +1,4 @@
+class Account{
+    public :wq
+            :
+}
--- thiskey.java
+++ thiskey.java
@@ -1,3 +1,18 @@
-public class thiskey {
+class Pen{
+    String Color;
+    String Type;
+    public void write(){
+        System.out.println("Write something!");
+    }
+    public void printcolor(){
+        System.out.println(this.Color);
+    }
+}
 
+public class thiskey {
+    public static void main(String args[]){
+        Pen obj = new Pen();
+        obj.Color = "blue";
+        obj.printcolor();
+    }
 }
