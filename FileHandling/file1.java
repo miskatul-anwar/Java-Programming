04c6930c18e45268ee8b87e1ea925d85d78a8d46
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-06-01 11:09:21 +0600

    Sat Jun  1 11:09:21 AM +06 2024

--- FileHandling/file1.java
+++ FileHandling/file1.java
@@ -1,26 +1,19 @@
-import java.io.BufferedWriter;
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.FileWriter;
+import java.io.*;
 
 public class file1 {
-  public static void main(String[] args) {
-    FileOutputStream fout = new FileOutputStream("./new.txt");
+  public static void main(String[] args) throws IOException {
+    FileOutputStream fout = new FileOutputStream("Test.txt");
     DataOutputStream out = new DataOutputStream(fout);
-    out.writeDouble(10.0);
-    // out.writeDouble(10.0);
-    // out.writeDouble(10.0);
-    // out.writeDouble(10.0);
-    // out.writeDouble(10.0);
+    out.writeDouble(98.6);
+    out.writeInt(1000);
+    out.writeBoolean(true);
     out.close();
-
-    FileInputStream fin = new FileInputStream("./new.txt");
+    FileInputStream fin = new FileInputStream("Test.txt");
     DataInputStream in = new DataInputStream(fin);
     double d = in.readDouble();
-    System.out.println(d);
+    int i = in.readInt();
+    boolean b = in.readBoolean();
+    System.out.println("Here are the values: " + d + " " + i + " " + b);
     in.close();
-    return;
   }
 }
