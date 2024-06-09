a5f32ce99a51534e29e5685ae4a47bef314dcfc4
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-06-09 21:38:56 +0600

    Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   case.java
    	modified:   input.txt

--- case.java
+++ case.java
@@ -1,3 +1,10 @@
 import java.util.Scanner;
-
-public class case{
+c/**
+ * case
+ */
+public class case {
+public static void main(String[] args) {
+    System.out.println("hi");
+    Scanner sc = new Scanner(System.in);
+  }
+}
