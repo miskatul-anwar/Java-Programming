ba80e71a5b4a47042bb4dccab5d953db3fdbbf8f
Miskatul Anwar <miskatul.anwar.csecu@gmail.com>
2024-09-23 08:54:22 +0600

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   Swing/GFG.class
    	new file:   Swing/GFG.java

---
+++ Swing/GFG.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import javax.swing.*;
+
+/**
+ * GFG
+ */
+public class GFG {
+
+  public static void main(String[] args) {
+    JFrame frame = new JFrame();
+    JButton button = new JButton("Click");
+    button.setBounds(150, 200, 220, 50);
+    frame.add(button);
+    frame.setSize(500, 600);
+    frame.setVisible(true);
+  }
+}
