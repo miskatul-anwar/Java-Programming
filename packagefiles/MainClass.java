94b3b950e8908451e5d046b9597250fff2f4c81b
miskatul-anwar <miskatul.anwar.csecu@gmail.com>
2024-12-25 09:51:52 +0600

    Auto-commit: Wed Dec 25 09:51:49 AM +06 2024

---
+++ packagefiles/MainClass.java
@@ -0,0 +1,13 @@
+import main1.c1;
+
+class c1 {
+
+  public void m1() {
+    System.out.println("m1 of c1");
+  }
+
+  public static void main(String args[]) {
+    c1 obj = new c1();
+    obj.m1();
+  }
+}
